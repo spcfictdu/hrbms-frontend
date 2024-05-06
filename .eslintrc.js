36c05874e5b8cb1f1990f2dd404c327e7041ed77
Gabriel Gatbonton <115710319+gabrielgatbonton@users.noreply.github.com>
2024-05-06 11:09:45 +0800

    Adjusted Some More Settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
   extends: [
     "plugin:vue/essential",
     "eslint:recommended",
-    "plugin:prettier/recommended",
   ],
   parserOptions: {
     parser: "@babel/eslint-parser",
@@ -14,11 +13,5 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "prettier/prettier": [
-      "error",
-      {
-        endOfLine: "auto",
-      },
-    ],
   },
 };
