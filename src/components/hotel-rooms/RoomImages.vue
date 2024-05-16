14bf4b93e531859b9accb77fb00a5dc4f0b40781
PrinceCharles47 <113660951+PrinceCharles47@users.noreply.github.com>
2024-05-17 07:29:26 +0800

    Room details view

---
+++ src/components/hotel-rooms/RoomImages.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <v-row dense>
+      <v-col cols="12">
+        <v-img
+          class="room-images"
+          height="300"
+          src="../../assets/hotelRoom.png"
+        />
+      </v-col>
+      <v-col cols="4">
+        <v-img class="room-images" height="100" src="../../assets/hotelRoom.png" />
+      </v-col>
+      <v-col cols="4">
+        <v-img class="room-images" height="100" src="../../assets/hotelRoom.png" />
+      </v-col>
+      <v-col cols="4">
+        <v-img class="room-images" height="100" src="../../assets/hotelRoom.png" />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RoomImages",
+  data: () => ({
+    room: {
+      name: "The Serenity Suite",
+      category: "Family Room",
+      maxOccupancy: 2,
+      amenities: [
+        "Spectacular views",
+        "Luxurious bedding",
+        "Modern bathroom with deluxe toiletries",
+        "Private outdoor space",
+        "Spectacular views",
+        "Luxurious bedding",
+        "Modern bathroom with deluxe toiletries",
+        "Private outdoor space",
+      ],
+    },
+  }),
+};
+</script>
+
+<style scoped>
+.room-images {
+  border-radius: 5px;
+}
+</style>
---
+++ src/views/hotel-rooms/RoomDetailsView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <HotelRoomsHeader />
+    <v-divider class="mb-10" />
+    <v-row>
+      <v-col cols="12" md="5">
+        <RoomImages />
+      </v-col>
+      <v-col cols="12" md="7">
+        <RoomDetails />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import HotelRoomsHeader from "../../components/headers/HotelRoomsHeader.vue";
+import RoomImages from "../../components/hotel-rooms/RoomImages.vue";
+import RoomDetails from "../../components/hotel-rooms/RoomDetails.vue";
+
+export default {
+  name: "RoomDetailsView",
+  components: { HotelRoomsHeader, RoomImages, RoomDetails },
+  data: () => ({
+    hello: "world",
+  }),
+};
+</script>
+
+<style scoped></style>
