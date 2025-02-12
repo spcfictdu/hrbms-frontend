7c22b926355dab4038770c1aedee2ce6ed523eb1
Gabriel Gatbonton <gabriel.gatbonton3262@gmail.com>
2025-02-12 08:58:24 +0800

    :wrench: Adjusted Route Config

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,23 @@ import guestUser from "./guest-user/guest-user";
 
 Vue.use(VueRouter);
 
+const mappedRoutes = {
+  publicRoutes: [
+    ...authentication.map(({ name }) => name),
+    ...publicRoutes[0].children.map(({ name }) => name),
+  ],
+  authRoutes: [...authentication.map(({ name }) => name)],
+  roleRoutes: {
+    GUEST: [...guestUser[0].children.map(({ name }) => name)],
+    ADMIN: [
+      ...dashboardRoutes.map(({ name }) => name),
+      ...roomsList[0].children.map(({ name }) => name),
+      ...guestList.map(({ name }) => name),
+      ...transaction.map(({ name }) => name),
+    ],
+  },
+};
+
 const routes = [
   ...dashboardRoutes,
   ...roomsList,
@@ -28,66 +45,45 @@ const router = new VueRouter({
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
-      return savedPosition
+      return savedPosition;
     } else {
-      return { x: 0, y: 0 }
+      return { x: 0, y: 0 };
     }
-  }
+  },
 });
 
 router.beforeEach((to, from, next) => {
   const loggedIn = auth.user();
   const userRole = auth.user() ? auth.user().role : null;
+  const DASHBOARD_NAME = {
+    GUEST: "Public Dashboard",
+    ADMIN: "Dashboard",
+  };
 
-  if (!loggedIn && !allowedRoutes("PUBLIC").includes(to.name)) {
-     next({ name: "Guest Dashboard" });
-  } else if (loggedIn && allowedRoutes("LOGIN").includes(to.name)) {
-    if (userRole === "GUEST") {
-       next({ name: "Guest Dashboard" });
-    } else if (userRole === "ADMIN") {
-       next({ name: "Dashboard" });
-    }
-  } else if (userRole === "GUEST" && !allowedRoutes("GUEST").includes(to.name)) {
-    next({ name: "Guest Dashboard" });
-  } else if (userRole === "ADMIN" && !allowedRoutes("ADMIN").includes(to.name)) {
-    next({ name: "Dashboard" });
-  } else {
-    next();
+  if (!loggedIn && !mappedRoutes.publicRoutes.includes(to.name)) {
+    next({ name: "Public Dashboard" });
+    return;
   }
+
+  if (loggedIn && mappedRoutes.authRoutes.includes(to.name)) {
+    next({ name: DASHBOARD_NAME[userRole] });
+    return;
+  }
+
+  const allowedRoutes = [...(mappedRoutes.roleRoutes[userRole] || [])];
+
+  if (!allowedRoutes.includes(to.name)) {
+    next({ name: DASHBOARD_NAME[userRole] });
+    return;
+  }
+
+  next();
 });
 
 router.afterEach((to) => {
   Vue.nextTick(() => {
-    document.title = `${to.name} | FCPC`;
-  })
-})
-
-function allowedRoutes (type) {
- let data = [];
-  if (type === "LOGIN") {
-    // Sign In Routes
-    data = ["Sign In", "Guest Sign In"];
-  } else if (type === "PUBLIC") {
-    // Public Routes
-    data = [
-      ...publicRoutes[0].children.filter(({ meta }) => meta.isPublic).map(({ name }) => name),
-      "Sign In", "Guest Sign In",
-    ]
-  } else if (type === "GUEST") {
-    // Guest User Routes
-    data = [
-      ...guestUser[0].children.map(({name}) => name),
-    ]
-  } else if (type === "ADMIN") {
-    // Admin User Routes
-    data = [
-      ...dashboardRoutes.map(({name}) => name),
-      ...roomsList[0].children.map(({name}) => name),
-      ...guestList.map(({name}) => name),
-      ...transaction.map(({name}) => name),
-    ]
-  }
- return data;
-}
+    document.title = `${to.name} | HRMS`;
+  });
+});
 
 export default router;
