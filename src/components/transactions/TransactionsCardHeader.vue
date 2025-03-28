c3fff2701bcb196f289482761c59eb2090c7203b
rndall <96964503+rndall@users.noreply.github.com>
2025-03-28 23:45:27 +0800

    :recycle: Refactor TransactionsCardHeader to reuse TableFilter component

--- src/components/transactions/TransactionsCardHeader.vue
+++ src/components/transactions/TransactionsCardHeader.vue
@@ -1,176 +1,119 @@
 <template>
-  <div>
-    <v-card-title class="text-subtitle-2 font-weight-black ml-3">
-      {{ transactions.pagination.total }} TRANSACTIONS
-      <v-spacer></v-spacer>
-      <div v-if="show" class="d-flex">
-        <div class="mr-2">
-          <v-btn
-            :ripple="false"
-            color="warning"
-            @click="clearQuery"
-            rounded
+  <TableFilter
+    :numberOfItems="transactions.pagination.total"
+    item="TRANSACTIONS"
+    :searchFn="searchFunction"
+    :clearFn="clearQuery"
+    :loaded="hasData"
+  >
+    <v-col cols="12" sm="6" md="4">
+      <span class="text-caption ml-2">First Name</span>
+      <v-text-field
+        outlined
+        dense
+        hide-details="auto"
+        v-model="query_params.firstName"
+      ></v-text-field>
+    </v-col>
+    <v-col cols="12" sm="6" md="4">
+      <span class="text-caption ml-2">Middle Name</span>
+      <v-text-field
+        outlined
+        dense
+        hide-details="auto"
+        v-model="query_params.middleName"
+      ></v-text-field>
+    </v-col>
+    <v-col cols="12" sm="6" md="4">
+      <span class="text-caption ml-2">Last Name</span>
+      <v-text-field
+        outlined
+        dense
+        hide-details="auto"
+        v-model="query_params.lastName"
+      ></v-text-field>
+    </v-col>
+    <v-col cols="12" sm="6" md="4">
+      <span class="text-caption ml-2">Reference</span>
+      <v-text-field
+        outlined
+        dense
+        hide-details="auto"
+        v-model="query_params.referenceNumber"
+      ></v-text-field>
+    </v-col>
+    <v-col cols="12" sm="6" md="4">
+      <span class="text-caption ml-2">Check-in Date</span>
+      <v-menu
+        v-model="menuCheckIn"
+        :close-on-content-click="false"
+        offset-y
+        transition="scale-transition"
+        min-width="auto"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
+            v-model="query_params.checkInDate"
+            v-on="on"
+            v-bind="attrs"
             outlined
-            class="d-none d-sm-flex"
+            dense
+            readonly
+            hide-details="auto"
+          >
+          </v-text-field>
+        </template>
+        <v-date-picker
+          v-model="query_params.checkInDate"
+          @input="menuCheckIn = false"
+        >
+          <v-btn color="primary" block @click="clearDate('Check In')"
             >Clear</v-btn
           >
-          <v-btn
-            :ripple="false"
-            color="warning"
-            class="d-flex d-sm-none"
-            @click="clearQuery"
-            icon
-            ><v-icon left>mdi-cached</v-icon></v-btn
-          >
-        </div>
-
-        <div>
-          <v-btn
-            color="primary"
-            rounded
-            class="d-none d-sm-flex"
-            @click="searchFunction"
-            ><v-icon left>mdi-magnify</v-icon>Search</v-btn
-          >
-          <v-btn
-            color="primary"
-            icon
-            class="d-flex d-sm-none"
-            @click="searchFunction"
-            ><v-icon>mdi-magnify</v-icon></v-btn
-          >
-        </div>
-      </div>
-
-      <v-btn
-        fab
-        outlined
-        @click="show = !show"
-        :small="small"
-        :x-small="xSmall"
-        color="primary"
-        class="ml-2"
+        </v-date-picker>
+      </v-menu>
+    </v-col>
+    <v-col cols="12" sm="6" md="4">
+      <span class="text-caption ml-2">Check-out Date</span>
+      <v-menu
+        v-model="menuCheckOut"
+        :close-on-content-click="false"
+        offset-y
+        transition="scale-transition"
+        min-width="auto"
       >
-        <v-icon>{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
-      </v-btn>
-    </v-card-title>
-    <v-row>
-      <v-col cols="12">
-        <v-progress-linear
-          color="amber"
-          :indeterminate="indeterminate"
-          :value="indeterminateValue"
-        ></v-progress-linear>
-      </v-col>
-    </v-row>
-    <v-row v-if="show" class="mx-3 text-subtitle-2">
-      <v-col cols="12" sm="6" md="4">
-        <span class="ml-2">First Name</span>
-        <v-text-field
-          outlined
-          dense
-          hide-details="auto"
-          v-model="query_params.firstName"
-        ></v-text-field>
-      </v-col>
-      <v-col cols="12" sm="6" md="4">
-        <span class="ml-2">Middle Name</span>
-        <v-text-field
-          outlined
-          dense
-          hide-details="auto"
-          v-model="query_params.middleName"
-        ></v-text-field>
-      </v-col>
-      <v-col cols="12" sm="6" md="4">
-        <span class="ml-2">Last Name</span>
-        <v-text-field
-          outlined
-          dense
-          hide-details="auto"
-          v-model="query_params.lastName"
-        ></v-text-field>
-      </v-col>
-      <v-col cols="12" sm="6" md="4">
-        <span class="ml-2">Reference</span>
-        <v-text-field
-          outlined
-          dense
-          hide-details="auto"
-          v-model="query_params.referenceNumber"
-        ></v-text-field>
-      </v-col>
-      <v-col cols="12" sm="6" md="4">
-        <span class="ml-2">Check-in Date</span>
-        <v-menu
-          v-model="menuCheckIn"
-          :close-on-content-click="false"
-          offset-y
-          transition="scale-transition"
-          min-width="auto"
-        >
-          <template v-slot:activator="{ on, attrs }">
-            <v-text-field
-              v-model="query_params.checkInDate"
-              v-on="on"
-              v-bind="attrs"
-              outlined
-              dense
-              readonly
-              hide-details="auto"
-            >
-            </v-text-field>
-          </template>
-          <v-date-picker
-            v-model="query_params.checkInDate"
-            @input="menuCheckIn = false"
-          >
-            <v-btn color="primary" block @click="clearDate('Check In')"
-              >Clear</v-btn
-            >
-          </v-date-picker>
-        </v-menu>
-      </v-col>
-      <v-col cols="12" sm="6" md="4">
-        <span class="ml-2">Check-out Date</span>
-        <v-menu
-          v-model="menuCheckOut"
-          :close-on-content-click="false"
-          offset-y
-          transition="scale-transition"
-          min-width="auto"
-        >
-          <template v-slot:activator="{ on, attrs }">
-            <v-text-field
-              v-model="query_params.checkOutDate"
-              v-on="on"
-              v-bind="attrs"
-              outlined
-              dense
-              readonly
-              hide-details="auto"
-            >
-            </v-text-field>
-          </template>
-          <v-date-picker
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
             v-model="query_params.checkOutDate"
-            @input="menuCheckOut = false"
+            v-on="on"
+            v-bind="attrs"
+            outlined
+            dense
+            readonly
+            hide-details="auto"
           >
-            <v-btn color="primary" block @click="clearDate('Check Out')"
-              >Clear</v-btn
-            >
-          </v-date-picker>
-        </v-menu>
-      </v-col>
-    </v-row>
-  </div>
+          </v-text-field>
+        </template>
+        <v-date-picker
+          v-model="query_params.checkOutDate"
+          @input="menuCheckOut = false"
+        >
+          <v-btn color="primary" block @click="clearDate('Check Out')"
+            >Clear</v-btn
+          >
+        </v-date-picker>
+      </v-menu>
+    </v-col>
+  </TableFilter>
 </template>
 
 <script>
+import TableFilter from "../table-headers/TableFilter.vue";
 import TablePagination from "@/mixins/TablePagination";
 
 export default {
   name: "TrasactionsCardHeader",
+  components: { TableFilter },
   props: {
     transactions: {
       type: Object,
@@ -233,6 +176,9 @@ export default {
   computed: {
     size() {
       return this.$vuetify.breakpoint;
+    },
+    hasData() {
+      return !!this.transactions ?? false;
     },
   },
   watch: {
