38f0d709f975ed6405dac36ac67797e3e904df07
rndall <96964503+rndall@users.noreply.github.com>
2025-05-17 12:11:29 +0800

    :coffee: Create AddOnItem and display add-ons

---
+++ src/components/hotel-rooms/addOns/AddOnItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-btn
+    class="capitalizeText text-subtitle-2 font-weight-regular"
+    height="70px"
+    depressed
+    block
+    color="lightBg"
+  >
+    <div>
+      <div>{{ addOn.name }}</div>
+      <div class="font-weight-bold text-body-1">₱{{ addOn.price }}</div>
+    </div>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: "AddOnItem",
+  props: {
+    addOn: Object,
+  },
+};
+</script>
--- src/components/hotel-rooms/amenities/AddOnsComponent.vue
+++ src/components/hotel-rooms/amenities/AddOnsComponent.vue
@@ -1,11 +1,35 @@
 <template>
   <div>
-    Add-ons
+    <v-row justify="start">
+      <v-col
+        v-for="addOn in addOns"
+        :key="addOn.name"
+        class="pb-3 pb-sm-2"
+        cols="12"
+        md="3"
+        sm="4"
+      >
+        <AddOnItem :addOn="addOn" />
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
+import { mapActions, mapState } from "vuex";
+import AddOnItem from "../addOns/AddOnItem.vue";
+
 export default {
   name: "AddOnsComponent",
+  components: { AddOnItem },
+  methods: {
+    ...mapActions("addOns", ["fetchAddOns"]),
+  },
+  computed: {
+    ...mapState("addOns", ["addOns"]),
+  },
+  created() {
+    this.fetchAddOns();
+  },
 };
 </script>
