d38165556f869f2f8c191166fe7a084caf017f20
rndall <96964503+rndall@users.noreply.github.com>
2025-06-16 15:40:33 +0800

    :coffee: Create show drawer details toggle

--- src/components/cashier/CashierDrawer.vue
+++ src/components/cashier/CashierDrawer.vue
@@ -5,30 +5,50 @@
       Cashier Drawer {{ drawerNumber }}
     </p>
 
-    <v-card flat class="d-flex flex-column flex-md-row ps-2 my-6">
-      <div>
-        <v-card-title class="font-weight-bold">First Floor Lobby</v-card-title>
-        <v-card-text>
-          LAST OPENED BY | <span class="font-weight-bold">S45</span>
-        </v-card-text>
+    <v-card flat class="my-6">
+      <div class="d-flex flex-column flex-md-row">
+        <div class="ps-2">
+          <v-card-title class="font-weight-bold"
+            >First Floor Lobby</v-card-title
+          >
+          <v-card-text>
+            LAST OPENED BY | <span class="font-weight-bold">S45</span>
+          </v-card-text>
+        </div>
+
+        <v-card-actions class="ml-md-auto align-start pa-4">
+          <v-btn
+            block
+            small
+            depressed
+            rounded
+            :color="showDetails ? 'lightBg' : 'primary'"
+            class="px-6"
+            @click="showDetails = !showDetails"
+          >
+            {{ showDetails ? "CLOSE" : "OPEN" }}
+          </v-btn>
+        </v-card-actions>
       </div>
 
-      <v-card-actions class="ml-md-auto align-start pa-4">
-        <v-btn block small depressed rounded color="primary" class="px-7"
-          >OPEN</v-btn
-        >
-      </v-card-actions>
+      <CashierDrawerDetails :showDetails="showDetails" />
     </v-card>
   </div>
 </template>
 
 <script>
+import CashierDrawerDetails from "./CashierDrawerDetails.vue";
+
 export default {
   name: "CashierDrawer",
+  components: { CashierDrawerDetails },
   props: {
     drawerNumber: String,
     isFirstRow: Boolean,
   },
+  data: () => ({
+    showDetails: false,
+  }),
 };
 </script>
 
---
+++ src/components/cashier/CashierDrawerDetails.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-expand-transition>
+    <div v-show="showDetails">
+      <v-divider></v-divider>
+
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col v-for="n in 10" :key="n" cols="6">
+              <div>Cash</div>
+              <div class="font-weight-bold">PHP 0.00</div>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+
+      <v-card-actions class="flex-column pa-6 pt-0">
+        <v-btn
+          block
+          color="primary"
+          :outlined="i < 1"
+          v-for="(button, i) in buttons"
+          :key="button.text"
+          class="mt-2 ms-0"
+        >
+          {{ button.text }}
+        </v-btn>
+      </v-card-actions>
+    </div>
+  </v-expand-transition>
+</template>
+
+<script>
+export default {
+  name: "CashierDrawerDetails",
+  props: {
+    showDetails: Boolean,
+  },
+  data: () => ({
+    buttons: [
+      {
+        text: "View Cashier History",
+      },
+      {
+        text: "View Cashier Drawer",
+      },
+    ],
+  }),
+};
+</script>
