185e702763f5756111a05381ebb3e68d9f211405
rndall <96964503+rndall@users.noreply.github.com>
2025-10-10 16:52:46 +0800

    :wrench: Change TitleSlot to a div element to reduce default margin

--- src/components/slots/TitleSlot.vue
+++ src/components/slots/TitleSlot.vue
@@ -1,6 +1,6 @@
 <template>
-  <p class="text-overline font-weight-bold text-uppercase ml-1 mt-1">
+  <div class="text-overline font-weight-bold text-uppercase ml-1 mt-1">
     <slot name="title"></slot>
     <slot />
-  </p>
+  </div>
 </template>
