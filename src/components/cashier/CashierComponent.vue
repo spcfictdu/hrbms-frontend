de70e62dfbff6d075f6950ef1fc29d4ffb4e855f
rndall <96964503+rndall@users.noreply.github.com>
2025-11-03 08:52:27 +0800

    :wrench: Refactor loading

--- src/components/cashier/CashierComponent.vue
+++ src/components/cashier/CashierComponent.vue
@@ -1,27 +1,10 @@
 <template>
-  <div v-if="loading">
-    <v-skeleton-loader type="heading" width="400" height="32" />
-    <v-skeleton-loader type="heading" width="600" />
-
-    <v-row>
-      <v-col cols="12" md="6">
-        <v-divider class="mt-12" />
-        <FormSection title="Guest Name">
-          <v-autocomplete
-            dense
-            hide-details
-            outlined
-            loading
-            disabled
-            class="mr-3"
-          />
-        </FormSection>
-      </v-col>
-    </v-row>
-  </div>
-
-  <div v-else>
-    <div>
+  <div>
+    <div v-if="loading">
+      <v-skeleton-loader type="heading" width="50%" height="32" />
+      <v-skeleton-loader type="heading" />
+    </div>
+    <div v-else>
       <div class="text-uppercase primary--text font-weight-bold text-h6">
         {{ cashierLocation(session.cashierId) }}
       </div>
@@ -54,6 +37,8 @@
               v-model="guestName"
               class="mr-3"
               clearable
+              :loading="getLoading('transactions')"
+              :disabled="getLoading('transactions')"
               @input="selectedTransaction = null"
               @keydown="selectedTransaction = null"
             />
@@ -88,7 +73,7 @@
 
           <v-divider></v-divider>
           <BookingSummary
-            :loading="transactionLoading.form"
+            :loading="getLoading('form')"
             :queryParams="receiptQuery"
             :clientMeta="clientMeta"
             :btnStyling="btnStyling"
@@ -108,7 +93,7 @@ import PaymentTemplate from "../form-templates/PaymentTemplate.vue";
 import GuestCard from "../guests/GuestCard.vue";
 import FormSection from "../sections/FormSection.vue";
 import cashierLocation from "@/mixins/cashier-location";
-import { mapActions, mapState, mapMutations } from "vuex";
+import { mapActions, mapState, mapMutations, mapGetters } from "vuex";
 
 export default {
   name: "CashierComponent",
@@ -301,7 +286,7 @@ export default {
   },
   computed: {
     ...mapState("transaction", ["transactions", "transaction"]),
-    ...mapState("transaction", { transactionLoading: "loading" }),
+    ...mapGetters("transaction", ["getLoading"]),
     ...mapState("vouchers", ["activeVoucher"]),
     ...mapState("roomEnum", ["room"]),
     formDetails() {
